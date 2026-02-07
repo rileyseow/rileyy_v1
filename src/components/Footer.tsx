176c3c8c832e3e7da2f1c9b81378f268acea8448
Riley Seow <rielsical@gmail.com>
2026-02-06 16:03:33 -0800

    PORTFOLIO-3: Tweak 'Projects' text and restyle footer
    Let's make the footer styling more consistent with the rest of the page, and add scrolling / shuffle functionality to the motto text banner. Let's also tweak the 'Projects' section text for brevity.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,50 @@
-import layerCake from "../assets/layerCake.png";
+import { useState } from "react";
+
+import ArrowsCrossedSvg from "../assets/arrowsCrossed.svg?react";
 
 import "./Footer.scss";
 
+const MOTTOS = [
+  "Anything can be learned!",
+  "And it's inside myself that I must create someone who will understand. -Clarice Lispector",
+  "You’re strange because you are not yourself and that’s what keeps you hidden. -Unknown",
+] as const;
+
 const Footer = () => {
+  const [mottoIdx, setMottoIdx] = useState<number>(0);
+
   return (
     <footer className="Footer">
-      <span className="text-banner">Anything can be learned!</span>
-      <img className="layer-cake" src={layerCake} />
+      <div className="text-attribution">
+        <span>&#169; 2026 Riley Seow</span>
+        <span>
+          Coded in VSCode with Typescript React. Deployed with Vercel. Typeface
+          is Fira Sans and Fira Mono via Google Fonts.
+        </span>
+      </div>
+      <div className="scrolling-motto">
+        <div className="motto-text-container">
+          <div key={`${MOTTOS[mottoIdx]}-one`} className="motto-text">
+            {MOTTOS[mottoIdx]}
+          </div>
+          {/* duplicate .motto-text with aria-hidden for infinite scroll effect */}
+          <div
+            key={`${MOTTOS[mottoIdx]}-two`}
+            className="motto-text"
+            aria-hidden
+          >
+            {MOTTOS[mottoIdx]}
+          </div>
+        </div>
+        <button
+          className="next-motto-button"
+          onClick={() =>
+            setMottoIdx((prevMottoIdx) => (prevMottoIdx + 1) % MOTTOS.length)
+          }
+        >
+          <ArrowsCrossedSvg />
+        </button>
+      </div>
     </footer>
   );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,10 +4,7 @@ const Projects = () => {
   return (
     <section className="Projects" id="Projects">
       <h2>Projects</h2>
-      <p className="coming-soon-text">
-        Coming soon! Currently redeploying a project. Also ramping up on some
-        Python and SQL to plan for something ✨in the works.✨
-      </p>
+      <p className="coming-soon-text">🚧 🔨</p>
     </section>
   );
 };
