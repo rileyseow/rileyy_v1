a99db037f61c918c1edb429a3d9878d1233241e7
Riley Seow <rielsical@gmail.com>
2026-04-02 17:21:53 -0700

    PORTFOLIO-27: Simplify Tech / Skills section. Regenerate package-lock.json to reflect changes from a few commits prior.

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,9 +1,7 @@
-import type { ReactNode } from 'react';
-import CssSvg from '../assets/Tech/css.svg?react';
+import { type ReactNode } from 'react';
+
 import D3Svg from '../assets/Tech/d3.svg?react';
 import ExpressSvg from '../assets/Tech/express.svg?react';
-import HtmlSvg from '../assets/Tech/html.svg?react';
-import JavascriptSvg from '../assets/Tech/javascript.svg?react';
 import MapboxSvg from '../assets/Tech/mapbox.svg?react';
 import NodeSvg from '../assets/Tech/node.svg?react';
 import PostgreSqlSvg from '../assets/Tech/postgresql.svg?react';
@@ -14,40 +12,50 @@ import TypescriptSvg from '../assets/Tech/typescript.svg?react';
 
 import './Tech.scss';
 
+/** @todo Implement toggle dev tool icon versus text on hover */
+
 interface DevTool {
   name: string;
   img?: ReactNode;
 }
 
-const DEV_TOOLS: DevTool[] = [
-  { name: 'Javascript (ES6+)', img: <JavascriptSvg /> },
-  { name: 'TypeScript', img: <TypescriptSvg /> },
-  { name: 'React', img: <ReactSvg /> },
-  { name: 'Redux', img: <ReduxSvg /> },
-  { name: 'HTML', img: <HtmlSvg /> },
-  {
-    name: 'CSS/SCSS',
-    img: [<CssSvg key='css' />, <ScssSvg key='scss' />],
-  },
-  { name: 'D3.js', img: <D3Svg /> },
-  { name: 'Mapbox', img: <MapboxSvg /> },
-  { name: 'Node.js', img: <NodeSvg /> },
-  { name: 'Express', img: <ExpressSvg /> },
-  { name: 'PostgreSQL', img: <PostgreSqlSvg /> },
-];
+const DEV_TOOLS: Record<string, DevTool[]> = {
+  'Front end': [
+    { name: 'TypeScript', img: <TypescriptSvg /> },
+    { name: 'React', img: <ReactSvg /> },
+    { name: 'Redux', img: <ReduxSvg /> },
+    { name: 'SCSS', img: <ScssSvg /> },
+    { name: 'D3', img: <D3Svg /> },
+    { name: 'Mapbox', img: <MapboxSvg /> },
+  ],
+  'Back end': [
+    { name: 'Node', img: <NodeSvg /> },
+    { name: 'Express', img: <ExpressSvg /> },
+    { name: 'PostgreSQL', img: <PostgreSqlSvg /> },
+  ],
+  Focus: [
+    { name: 'Real-time systems' },
+    { name: 'Data visualization' },
+    { name: 'Performance' },
+  ],
+};
 
 const Tech = () => {
   return (
     <section className='Tech'>
-      <h3>Lately I've been working in</h3>
-      <div className='tileset'>
-        {DEV_TOOLS.map(({ name, img }) => (
-          <div className='tile' key={name}>
-            {img}
-            <span className='name'>{name}</span>
-          </div>
-        ))}
-      </div>
+      <h3>Skills</h3>
+      {(
+        Object.entries(DEV_TOOLS) as [string, DevTool[]][]
+      ).map(([name, devTools]) => (
+        <div key={name} className='tech-category'>
+          <span className='category-name'>{name}:</span>
+          <ul className='category-list'>
+            {devTools.map(devTool => (
+              <li key={devTool.name}>{devTool.name}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </section>
   );
 };
